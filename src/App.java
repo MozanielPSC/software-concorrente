c171345e6d88bc6cc6d3e2870c59a13549579ca8
Mozaniel Pereira e Silva Corrêa <mozanielpcorrea@gmail.com>
2022-07-24 18:21:29 -0300

    FIrst commit

---
+++ src/App.java
@@ -0,0 +1,25 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class App {
+    public static void main(String[] args) throws Exception {
+       getConnection();
+    }
+
+public static Connection getConnection() throws Exception{
+  try{
+   String driver = "com.mysql.cj.jdbc.Driver";
+   String url = "jdbc:mysql://=localhost:3306/sergio_db";
+   String username = "sergio";
+   String password = "password";
+   Class.forName(driver);
+   
+   Connection conn = DriverManager.getConnection(url,username,password);
+   System.out.println("Connected");
+   return conn;
+  } catch(Exception e){System.out.println(e);}
+  
+  
+  return null;
+ }
+}
