35e960ed53c1b73267b5da8c09ae8d03ec2400ae
Felipe Kafuri <felipe11.rk@gmail.com>
2022-07-24 18:28:53 -0300

    .

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.DriverManager;  
 public class App {
     public static void main(String[] args) throws Exception {
        getConnection();
